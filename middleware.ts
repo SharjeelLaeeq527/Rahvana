6978c0795159cf92ed499e7afb67d242f9cdd028
Ghulam-Ahmed-1357 <ghulamahmedmbs@gmail.com>
2026-03-02 12:41:57 +0500

    login issue fix

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,14 @@
-import { createServerClient } from '@supabase/ssr'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
+  // eslint-disable-next-line prefer-const
+  let res = NextResponse.next({
+    request: {
+      headers: req.headers,
+    },
+  });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -12,19 +17,33 @@ export async function middleware(req: NextRequest) {
       cookies: {
         getAll: () => req.cookies.getAll(),
         setAll: (cookies) => {
+          cookies.forEach(({ name, value }) => req.cookies.set(name, value));
           cookies.forEach(({ name, value, options }) => {
             res.cookies.set(name, value, {
               ...options,
-              sameSite: 'lax',
+              sameSite: "lax",
               secure: true,
-              path: '/',
-            })
-          })
+              path: "/",
+            });
+          });
         },
       },
-    }
-  )
+    },
+  );
 
-  await supabase.auth.getSession()
-  return res
+  await supabase.auth.getSession();
+  return res;
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
