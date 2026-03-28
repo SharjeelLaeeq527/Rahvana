015b4b0813bec9351be832673ea931d93e9e9fc3
Ghulam-Ahmed-1357 <ghulamahmedmbs@gmail.com>
2026-03-28 15:10:34 +0500

    google analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { ClientHeader } from "./components/layout/ClientHeader";
 import { AuthProvider } from "./context/AuthContext";
@@ -29,6 +30,19 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning className="bg-background">
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-TNW3YZDTCR"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-TNW3YZDTCR');
+          `}
+        </Script>
         <ToastProvider>
           <AuthProvider>
             <LanguageProvider>
